9cb8047f8838b8cff0f92ad93904e08f221c2d7d
Gabriele <gabrieleberti18@gmail.com>
2022-07-11 17:23:30 +0200

    implementing movement and animation of the game character

--- GameCharacter.cpp
+++ GameCharacter.cpp
@@ -1,9 +1,26 @@
-//
-// Created by lorenzoleuter on 06/07/22.
-//
-
 #include "GameCharacter.h"
+
 
 void GameCharacter::getKilled(){
 
+}
+
+GameCharacter::GameCharacter() {
+    texture.loadFromFile("C:\\Users\\gabri\\CLionProjects\\FinnRunner\\assets\\FinnSprite.png");
+    game_character.setTexture(texture);
+    //game_character.setTextureRect();
+    pos = {0.0f, 0.0f}; // pos = {"spawn coordinates"}
+}
+
+
+
+void GameCharacter::setDirection(const sf::Vector2f &dir) {
+
+    vel = dir * speed;
+
+}
+
+void GameCharacter::Update(float dt) {
+    pos += vel * dt;
+    game_character.setPosition(pos);
 }
--- GameCharacter.h
+++ GameCharacter.h
@@ -1,15 +1,22 @@
-//
-// Created by lorenzoleuter on 06/07/22.
-//
-
 #ifndef FINN_RUNNER_GAMECHARACTER_H
 #define FINN_RUNNER_GAMECHARACTER_H
-#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics.hpp>
 
-class GameCharacter : public sf::Sprite{
+class GameCharacter{
     public:
+        GameCharacter();
         void getKilled();
+        void setDirection(const sf::Vector2f& dir);
+        void Update(float dt);
+        sf::Sprite getGameCharacter(){
+            return game_character;
+        }
     private:
+        float speed = 1.0f;
+        sf::Vector2f pos;
+        sf::Vector2f vel = {0.0f, 0.0f};
+        sf::Texture texture;
+        sf::Sprite game_character;
 
 
 };
