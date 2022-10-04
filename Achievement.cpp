0e2acb31ed5a6eb5f291e20437a7353f87d11008
Lorenzo Leuter <lorenzo.leuter.stud@unifi.it>
2022-10-04 21:24:06 +0200

    Implement Achievement and use observer for achievement

---
+++ Achievement.cpp
@@ -0,0 +1,24 @@
+//
+// Created by lori0 on 04/10/2022.
+//
+
+#include "Achievement.h"
+
+Achievement::Achievement(GameData *gameData): g(gameData),score(0) {
+    g->registerObserver(this);
+}
+
+Achievement::~Achievement() {
+    g->removeObserver(this);
+}
+
+void Achievement::update() {
+    score = g->getMeters();
+    draw();
+}
+
+void Achievement::draw() {
+    if(score == 15){
+        g->setAchievementTxt("HELLO GUYSSSSSS");
+    }
+}
